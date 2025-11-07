4e3c9db54a7c32c4c122d1ea9b2a72f7041b986a
BoWei <781035861@qq.com>
2025-11-07 15:38:19 +0800

    修改服务器配置的文案为简体中文

--- src/main/java/launch/groups/setting/ServerConfigGroup.java
+++ src/main/java/launch/groups/setting/ServerConfigGroup.java
@@ -11,37 +11,38 @@ public class ServerConfigGroup extends AbstractConfigGroup {
 
 
     public ServerConfigGroup(ConfigPanel owner) {
-        super(owner, "伺服端配置");
+        super(owner, "服务端配置");
     }
 
     @Override
     public Component getPreview() {
-
-        TitleWebPanel titleWebPanel1 = new TitleWebPanel("基本設置");
+        TitleWebPanel titleWebPanel1 = new TitleWebPanel("基本设置");
         titleWebPanel1.add(new GroupPanel(false,
-                new ConfigComponent("遊戲名稱", "login.server.name", ServerConfig.LOGIN_SERVERNAME),
-                new ConfigComponent("遊戲補丁版本", "login.server.minor", ServerConfig.MapleMinor),
-                new ConfigComponent("頻道選擇界面公告", "login.server.eventmessage", ServerConfig.LOGIN_EVENTMESSAGE),
+                new ConfigComponent("游戏名称", "login.server.name", ServerConfig.LOGIN_SERVERNAME),
+                new ConfigComponent("游戏补丁版本", "login.server.minor", ServerConfig.MapleMinor),
+                new ConfigComponent("频道选择界面公告", "login.server.eventmessage", ServerConfig.LOGIN_EVENTMESSAGE),
                 new GroupPanel(
-                        new ConfigComponent("連線IP(留空自動取得)", "server.host", "127.0.0.1"),
-                        new ConfigComponent("強制外網", "server.forceInternetIP", false)
+                        new ConfigComponent("连接服务器Ip(留空自动获取)", "server.host", "127.0.0.1"),
+                        new ConfigComponent("强制外网", "server.forceInternetIP", false)
                 ),
-                new ConfigComponent("頻道數量", "channel.server.ports", ServerConfig.CHANNEL_PORTS),
-                new ConfigComponent("遊戲伺服器名稱", "login.server.flag", ServerConfig.LOGIN_SERVERFLAG, getAllServerName()),
-                new ConfigComponent("遊戲伺服器狀態", "login.server.status", ServerConfig.LOGIN_SERVERSTATUS, getAllServerStatus()),
+                new ConfigComponent("频道数量", "channel.server.ports", ServerConfig.CHANNEL_PORTS),
+                new ConfigComponent("游戏服务器名称", "login.server.flag", ServerConfig.LOGIN_SERVERFLAG,
+                        getAllServerName()),
+                new ConfigComponent("游戏服务器状态", "login.server.status", ServerConfig.LOGIN_SERVERSTATUS,
+                        getAllServerStatus()),
                 new GroupPanel(
-                        new ConfigComponent("測試機", "tespia", ServerConfig.TESPIA),
-                        new ConfigComponent("多人測試限制(僅測試機)", "testpia.multiplayer", ServerConfig.MULTIPLAYER_TEST)
+                        new ConfigComponent("测试机", "tespia", ServerConfig.TESPIA),
+                        new ConfigComponent("多人测试限制(仅测试机)", "testpia.multiplayer", ServerConfig.MULTIPLAYER_TEST)
                 )
         ).setMargin(5));
 
-        TitleWebPanel titleWebPanel2 = new TitleWebPanel("資料庫連接設置");
+        TitleWebPanel titleWebPanel2 = new TitleWebPanel("数据库连接设置");
         titleWebPanel2.add(new GroupPanel(false,
                 new ConfigComponent("IP", "db.ip", ServerConfig.DB_IP),
-                new ConfigComponent("連接埠", "db.port", ServerConfig.DB_PORT),
-                new ConfigComponent("資料庫名稱", "db.name", ServerConfig.DB_NAME),
-                new ConfigComponent("帳號", "db.user", ServerConfig.DB_USER),
-                new ConfigComponent("密碼", "db.password", ServerConfig.DB_PASSWORD)
+                new ConfigComponent("连接端口", "db.port", ServerConfig.DB_PORT),
+                new ConfigComponent("数据库名称", "db.name", ServerConfig.DB_NAME),
+                new ConfigComponent("账号", "db.user", ServerConfig.DB_USER),
+                new ConfigComponent("密码", "db.password", ServerConfig.DB_PASSWORD)
 //                new GroupPanel(new WebButton("測試連接") {
 //                    {
 //                        setPreferredWidth(100);
@@ -69,10 +70,10 @@ public class ServerConfigGroup extends AbstractConfigGroup {
 //                }).setMargin(0, 150, 0, 0)
         ).setMargin(5));
 
-        TitleWebPanel titleWebPanel3 = new TitleWebPanel("其他設置");
+        TitleWebPanel titleWebPanel3 = new TitleWebPanel("其他设置");
         titleWebPanel3.add(new GroupPanel(false,
-                new ConfigComponent("腳本位置", "world.server.scriptspath", ServerConfig.WORLD_SCRIPTSPATH),
-                new ConfigComponent("客制化腳本位置", "world.server.scriptspath2", ServerConfig.WORLD_SCRIPTSPATH2)
+                new ConfigComponent("脚本位置", "world.server.scriptspath", ServerConfig.WORLD_SCRIPTSPATH),
+                new ConfigComponent("客制化脚本位置", "world.server.scriptspath2", ServerConfig.WORLD_SCRIPTSPATH2)
         ).setMargin(5));
 
         WebScrollPane webScrollPane = new WebScrollPane(new GroupPanel(5, false, titleWebPanel1, titleWebPanel2, titleWebPanel3), false);
